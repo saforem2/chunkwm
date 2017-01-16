8992927281f6e22bbfc7fd3d0d8a2e2cf4cdda8a
koekeishiya <aasvi93@hotmail.com>
2017-01-16 23:33:20 +0100

    update testplugin

--- src/plugins/testplugin/testplugin.cpp
+++ src/plugins/testplugin/testplugin.cpp
@@ -6,6 +6,7 @@
 #include "../../common/accessibility/application.h"
 #include "../../common/accessibility/observer.h"
 #include "../../common/dispatch/carbon.h"
+#include "../../common/dispatch/workspace.h"
 
 inline bool
 StringsAreEqual(const char *A, const char *B)
@@ -75,6 +76,20 @@ void ApplicationLaunchedHandler(const char *Data, unsigned int DataSize)
     });
 }
 
+void ApplicationUnhiddenHandler(const char *Data, unsigned int DataSize)
+{
+    workspace_application_details *Info =
+        (workspace_application_details *) Data;
+    printf("inside plugin: unhidden: '%s'\n", Info->ProcessName);
+}
+
+void ApplicationHiddenHandler(const char *Data, unsigned int DataSize)
+{
+    workspace_application_details *Info =
+        (workspace_application_details *) Data;
+    printf("inside plugin: hidden: '%s'\n", Info->ProcessName);
+}
+
 /*
  * NOTE(koekeishiya): Function parameters
  * plugin *Plugin
@@ -91,6 +106,16 @@ PLUGIN_MAIN_FUNC(PluginMain)
         if(StringsAreEqual(Node, "chunkwm_export_application_launched"))
         {
             ApplicationLaunchedHandler(Data, DataSize);
+            return true;
+        }
+        else if(StringsAreEqual(Node, "chunkwm_export_application_unhidden"))
+        {
+            ApplicationUnhiddenHandler(Data, DataSize);
+            return true;
+        }
+        else if(StringsAreEqual(Node, "chunkwm_export_application_hidden"))
+        {
+            ApplicationHiddenHandler(Data, DataSize);
             return true;
         }
     }
