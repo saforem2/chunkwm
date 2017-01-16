d40d16a6993a206122fc15e706aa059327eff3ae
koekeishiya <aasvi93@hotmail.com>
2017-01-16 14:39:49 +0100

    add source files

--- src/testplugin/build.sh
+++ src/testplugin/build.sh
@@ -1,1 +1,1 @@
-g++ testplugin.cpp -shared -o ../../plugins/testplugin.so -fPIC
+g++ testplugin.cpp ../common/accessibility/application.cpp ../common/accessibility/observer.cpp -shared -o ../../plugins/testplugin.so -fPIC -framework Carbon -framework ApplicationServices
