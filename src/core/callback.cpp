094e3a61bc3e9e19adefc9e7d8587c93081a7863
koekeishiya <aasvi93@hotmail.com>
2017-01-22 01:15:00 +0100

    implement #5 - multithread event dispatch

--- src/core/callback.cpp
+++ src/core/callback.cpp
@@ -1,8 +1,13 @@
-#include <stdio.h>
+#include "callback.h"
+#include "plugin.h"
+#include "wqueue.h"
+
 #include "dispatch/carbon.h"
 #include "dispatch/workspace.h"
 #include "dispatch/event.h"
-#include "plugin.h"
+
+#include <stdio.h>
+#include <pthread.h>
 
 #define ProcessPluginList(plugin_export)                   \
     plugin_list *List = BeginPluginList(plugin_export);    \
@@ -17,6 +22,64 @@
     }                                                      \
     EndPluginList(plugin_export)
 
+#define ProcessPluginListThreaded(plugin_export)           \
+    plugin_list *List = BeginPluginList(plugin_export);    \
+    plugin_work WorkArray[List->size()];                   \
+    int WorkCount = 0;                                     \
+    for(plugin_list_iter It = List->begin();               \
+        It != List->end();                                 \
+        ++It)                                              \
+    {                                                      \
+        plugin_work *Work = WorkArray + WorkCount++;       \
+        Work->Plugin = It->first;                          \
+        Work->Export = (char *) #plugin_export;            \
+        Work->Data = (char *) Event->Context;              \
+        AddWorkQueueEntry(&Queue,                          \
+                          &PluginWorkCallback,             \
+                          Work);                           \
+    }                                                      \
+    CompleteWorkQueue(&Queue);                             \
+    EndPluginList(plugin_export)                           \
+
+#define internal static
+
+struct plugin_work
+{
+    plugin *Plugin;
+    char *Export;
+    char *Data;
+};
+
+internal work_queue Queue;
+
+internal
+WORK_QUEUE_CALLBACK(PluginWorkCallback)
+{
+    plugin_work *Work = (plugin_work *) Data;
+
+    Work->Plugin->Run(Work->Export,
+                      Work->Data,
+                      sizeof(*Work->Data));
+}
+
+void BeginCallbackThreads(int Count)
+{
+    if((Queue.Semaphore = sem_open("work_queue_semaphore", O_CREAT, 0644, 0)) == SEM_FAILED)
+    {
+        fprintf(stderr,
+                "chunkwm: could not create semaphore for work queue, "
+                "multi-threading disabled!\n");
+        return;
+    }
+
+    pthread_t Thread[Count];
+    for(int Index = 0;
+        Index < Count;
+        ++Index)
+    {
+        pthread_create(&Thread[Count], NULL, &WorkQueueThreadProc, &Queue);
+    }
+}
 
 // NOTE(koekeishiya): Application-related callbacks.
 CHUNKWM_CALLBACK(Callback_ChunkWM_ApplicationLaunched)
@@ -25,7 +88,11 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_ApplicationLaunched)
         (carbon_application_details *) Event->Context;
     printf("%d: Launched '%s'\n", Info->PID, Info->ProcessName);
 
+#if 0
     ProcessPluginList(chunkwm_export_application_launched);
+#else
+    ProcessPluginListThreaded(chunkwm_export_application_launched);
+#endif
 
     EndCarbonApplicationDetails(Info);
 }
@@ -36,7 +103,11 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_ApplicationTerminated)
         (carbon_application_details *) Event->Context;
     printf("%d: Terminated '%s'\n", Info->PID, Info->ProcessName);
 
+#if 0
     ProcessPluginList(chunkwm_export_application_terminated);
+#else
+    ProcessPluginListThreaded(chunkwm_export_application_terminated);
+#endif
 
     EndCarbonApplicationDetails(Info);
 }
@@ -47,7 +118,11 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_ApplicationActivated)
         (workspace_application_details *) Event->Context;
     printf("%d: Activated '%s'\n", Info->PID, Info->ProcessName);
 
+#if 0
     ProcessPluginList(chunkwm_export_application_activated);
+#else
+    ProcessPluginListThreaded(chunkwm_export_application_activated);
+#endif
 
     EndWorkspaceApplicationDetails(Info);
 }
@@ -58,7 +133,11 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_ApplicationDeactivated)
         (workspace_application_details *) Event->Context;
     printf("%d: Deactivated '%s'\n", Info->PID, Info->ProcessName);
 
+#if 0
     ProcessPluginList(chunkwm_export_application_deactivated);
+#else
+    ProcessPluginListThreaded(chunkwm_export_application_deactivated);
+#endif
 
     EndWorkspaceApplicationDetails(Info);
 }
@@ -69,7 +148,11 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_ApplicationVisible)
         (workspace_application_details *) Event->Context;
     printf("%d: Show '%s'\n", Info->PID, Info->ProcessName);
 
+#if 0
     ProcessPluginList(chunkwm_export_application_unhidden);
+#else
+    ProcessPluginListThreaded(chunkwm_export_application_unhidden);
+#endif
 
     EndWorkspaceApplicationDetails(Info);
 }
@@ -80,7 +163,11 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_ApplicationHidden)
         (workspace_application_details *) Event->Context;
     printf("%d: Hide '%s'\n", Info->PID, Info->ProcessName);
 
+#if 0
     ProcessPluginList(chunkwm_export_application_hidden);
+#else
+    ProcessPluginListThreaded(chunkwm_export_application_hidden);
+#endif
 
     EndWorkspaceApplicationDetails(Info);
 }
@@ -89,8 +176,12 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_SpaceChanged)
 {
     /* NOTE(koekeishiya): The expanded macro expects a variable 'Info'.
      * This event does not take an argument, so we pass NULL. */
+#if 0
     char *Info = NULL;
     ProcessPluginList(chunkwm_export_space_changed);
+#else
+    ProcessPluginListThreaded(chunkwm_export_space_changed);
+#endif
 }
 
 // NOTE(koekeishiya): Display-related callbacks
@@ -113,4 +204,3 @@ CHUNKWM_CALLBACK(Callback_ChunkWM_DisplayResized)
 CHUNKWM_CALLBACK(Callback_ChunkWM_DisplayChanged)
 {
 }
-
--- src/core/chunkwm.cpp
+++ src/core/chunkwm.cpp
@@ -5,10 +5,10 @@
 #include "dispatch/workspace.h"
 #include "dispatch/event.h"
 
+#include "callback.h"
 #include "plugin.h"
 
 extern "C" void NSApplicationLoad();
-
 int main(int Count, char **Args)
 {
     NSApplicationLoad();
@@ -16,10 +16,12 @@ int main(int Count, char **Args)
     carbon_event_handler Carbon = {};
     if(BeginCarbonEventHandler(&Carbon))
     {
+        BeginCallbackThreads(4);
         BeginPlugins();
         BeginSharedWorkspace();
         StartEventLoop();
         CFRunLoopRun();
+
     }
 
     return EXIT_SUCCESS;
