294660b2e4c9182ae77d05c07e68b4696779d604
koekeishiya <aasvi93@hotmail.com>
2017-02-19 22:23:05 +0100

    tweak cvar names

--- src/plugins/tiling/constants.h
+++ src/plugins/tiling/constants.h
@@ -1,13 +1,13 @@
 #ifndef PLUGIN_CONSTANTS_H
 #define PLUGIN_CONSTANTS_H
 
-#define CVAR_SPACE_MODE             "global_virtual_space_mode"
+#define CVAR_SPACE_MODE             "global_space_mode"
 
-#define CVAR_SPACE_OFFSET_TOP       "global_virtual_space_offset_top"
-#define CVAR_SPACE_OFFSET_BOTTOM    "global_virtual_space_offset_bottom"
-#define CVAR_SPACE_OFFSET_LEFT      "global_virtual_space_offset_left"
-#define CVAR_SPACE_OFFSET_RIGHT     "global_virtual_space_offset_right"
-#define CVAR_SPACE_OFFSET_GAP       "global_virtual_space_offset_gap"
+#define CVAR_SPACE_OFFSET_TOP       "global_space_offset_top"
+#define CVAR_SPACE_OFFSET_BOTTOM    "global_space_offset_bottom"
+#define CVAR_SPACE_OFFSET_LEFT      "global_space_offset_left"
+#define CVAR_SPACE_OFFSET_RIGHT     "global_space_offset_right"
+#define CVAR_SPACE_OFFSET_GAP       "global_space_offset_gap"
 
 #define CVAR_BSP_SPAWN_LEFT         "bsp_spawn_left"
 #define CVAR_BSP_OPTIMAL_RATIO      "bsp_optimal_ratio"
