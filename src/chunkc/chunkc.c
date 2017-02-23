76787f22574755bae3a6731a6c50b0d56f2b053f
koekeishiya <aasvi93@hotmail.com>
2017-02-23 11:26:51 +0100

    cleanup

--- src/chunkc/chunkc.c
+++ src/chunkc/chunkc.c
@@ -9,66 +9,78 @@
 #include <netdb.h>
 #include <unistd.h>
 
+// NOTE(koekeishiya): 4131 is the port used by chwm tiling plugin.
+#define FALLBACK_PORT 4131
+
 int main(int Count, char **Argv)
 {
-    size_t Length = 0;
-    char *Temp, *Command;
-
-    for(unsigned Index = 1; Index < Count; ++Index)
+    if(Count < 2)
     {
-        Length += strlen(Argv[Index]);
+        fprintf(stderr, "chunkc: no arguments found!\n");
+        exit(1);
     }
 
-    Temp = Command = (char *) malloc(Length + Count - 1);
-
-    for(unsigned Index = 1; Index < Count; ++Index)
-    {
-        memcpy(Temp, Argv[Index], strlen(Argv[Index]));
-        Temp += strlen(Argv[Index]);
-        *Temp++ = ' ';
-    }
-    *(Temp - 1) = '\0';
+    int Port;
 
     char *PortEnv = getenv("CHUNKC_SOCKET");
     if(PortEnv)
     {
-        int SockFD, Port;
-        struct sockaddr_in srv_addr;
-        struct hostent *server;
-
         sscanf(PortEnv, "%d", &Port);
-
-        if((SockFD = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-        {
-            fprintf(stderr, "chunkc: could not create socket!\n");
-            exit(1);
-        }
-
-        server = gethostbyname("localhost");
-        srv_addr.sin_family = AF_INET;
-        srv_addr.sin_port = htons(Port);
-        memcpy(&srv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-        memset(&srv_addr.sin_zero, '\0', 8);
-
-        if(connect(SockFD, (struct sockaddr*) &srv_addr, sizeof(struct sockaddr)) == -1)
-        {
-            fprintf(stderr, "chunkc: connection failed!\n");
-            exit(1);
-        }
-
-        if(send(SockFD, Command, strlen(Command), 0) == -1)
-        {
-            fprintf(stderr, "chunkc: failed to send data!\n");
-        }
-
-        free(Command);
-        shutdown(SockFD, SHUT_RDWR);
-        close(SockFD);
     }
     else
     {
-        fprintf(stderr, "chunkc: 'env CHUNKC_SOCKET' not set!\n");
+        Port = FALLBACK_PORT;
     }
+
+    int SockFD;
+    struct sockaddr_in srv_addr;
+    struct hostent *server;
+
+    if((SockFD = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        fprintf(stderr, "chunkc: could not create socket!\n");
+        exit(1);
+    }
+
+    server = gethostbyname("localhost");
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = htons(Port);
+    memcpy(&srv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    memset(&srv_addr.sin_zero, '\0', 8);
+
+    if(connect(SockFD, (struct sockaddr*) &srv_addr, sizeof(struct sockaddr)) == -1)
+    {
+        fprintf(stderr, "chunkc: connection failed!\n");
+        exit(1);
+    }
+
+    size_t CommandLength = Count - 1;
+    size_t ArgLength[Count];
+
+    for(size_t Index = 1; Index < Count; ++Index)
+    {
+        ArgLength[Index] = strlen(Argv[Index]);
+        CommandLength += ArgLength[Index];
+    }
+
+    char Command[CommandLength];
+    char *Temp = Command;
+
+    for(size_t Index = 1; Index < Count; ++Index)
+    {
+        memcpy(Temp, Argv[Index], ArgLength[Index]);
+        Temp += ArgLength[Index];
+        *Temp++ = ' ';
+    }
+    *(Temp - 1) = '\0';
+
+    if(send(SockFD, Command, CommandLength, 0) == -1)
+    {
+        fprintf(stderr, "chunkc: failed to send data!\n");
+    }
+
+    shutdown(SockFD, SHUT_RDWR);
+    close(SockFD);
 
     return 0;
 }
