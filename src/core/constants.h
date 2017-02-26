213df0182c9f5cdfc43ad4bb2c456220c82bdad4
koekeishiya <aasvi93@hotmail.com>
2017-02-26 12:30:49 +0100

    bump version 0.0.5

--- src/core/constants.h
+++ src/core/constants.h
@@ -5,7 +5,7 @@
 
 #define CHUNKWM_MAJOR           0
 #define CHUNKWM_MINOR           0
-#define CHUNKWM_PATCH           4
+#define CHUNKWM_PATCH           5
 
 #define CHUNKWM_CONFIG          ".chunkwmrc"
 #define CHUNKWM_PORT            3920
