7fba9bfba09fd2c5d4101384d81474efd13ceda7
koekeishiya <aasvi93@hotmail.com>
2017-05-06 11:28:51 +0200

    core: refactor config code

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -10,10 +10,61 @@
 #include <stdio.h>
 #include <string.h>
 
+#define internal static
+
+// NOTE(koekeishiya): The char * we return points to a location
+// in the provided absolutepath string, thus, it should NOT be freed.
+char *PluginFilenameFromAbsolutepath(char *Absolutepath)
+{
+    char *Filename = NULL;
+    char *LastSlash = strrchr(Absolutepath, '/');
+
+    if(LastSlash)
+    {
+        char *Extension = strrchr(Absolutepath, '.');
+        if((Extension) && (strcmp(Extension, ".so") == 0))
+        {
+            Filename = LastSlash + 1;
+        }
+    }
+
+    return Filename;
+}
+
+// NOTE(koekeishiya): Caller is responsible for freeing memory of returned pointer
+internal char *
+PluginAbsolutepathFromDirectory(char *Filename, char *Directory)
+{
+    char *Absolutepath = NULL;
+    char *Extension = strrchr(Filename, '.');
+
+    if((Extension) && (strcmp(Extension, ".so") == 0))
+    {
+        size_t DirectoryLength = strlen(Directory);
+        size_t FilenameLength = strlen(Filename);
+        size_t TotalLength = DirectoryLength + 1 + FilenameLength;
+
+        Absolutepath = (char *) malloc(TotalLength + 1);
+        Absolutepath[TotalLength] = '\0';
+
+        memcpy(Absolutepath, Directory, DirectoryLength);
+        memset(Absolutepath + DirectoryLength, '/', 1);
+        memcpy(Absolutepath + DirectoryLength + 1, Filename, FilenameLength);
+    }
+
+    return Absolutepath;
+}
+
 DAEMON_CALLBACK(DaemonCallback)
 {
     token Type = GetToken(&Message);
-    if(TokenEquals(Type, CVAR_PLUGIN_HOTLOAD))
+    if(TokenEquals(Type, CVAR_PLUGIN_DIR))
+    {
+        token Token = GetToken(&Message);
+        char *Directory = TokenToString(Token);
+        UpdateCVar(CVAR_PLUGIN_DIR, Directory);
+    }
+    else if(TokenEquals(Type, CVAR_PLUGIN_HOTLOAD))
     {
         token Token = GetToken(&Message);
         int Status = TokenToInt(Token);
@@ -26,21 +77,28 @@ DAEMON_CALLBACK(DaemonCallback)
         if(Directory)
         {
             char *Filename = TokenToString(Token);
-            char *Extension = strrchr(Filename, '.');
-
-            if((Extension) && (strcmp(Extension, ".so") == 0))
+            char *Absolutepath = PluginAbsolutepathFromDirectory(Filename, Directory);
+            if(Absolutepath)
             {
-                size_t DirectoryLength = strlen(Directory);
-                size_t FilenameLength = strlen(Filename);
-                size_t TotalLength = DirectoryLength + 1 + FilenameLength;
-
-                char Absolutepath[TotalLength + 1];
-                Absolutepath[TotalLength] = '\0';
-
-                memcpy(Absolutepath, Directory, DirectoryLength);
-                memset(Absolutepath + DirectoryLength, '/', 1);
-                memcpy(Absolutepath + DirectoryLength + 1, Filename, FilenameLength);
-
+                struct stat Buffer;
+                if(stat(Absolutepath, &Buffer) == 0)
+                {
+                    LoadPlugin(Absolutepath, Filename);
+                }
+                else
+                {
+                    fprintf(stderr, "chunkwm: plugin '%s' not found..\n", Absolutepath);
+                }
+                free(Absolutepath);
+            }
+            free(Filename);
+        }
+        else
+        {
+            char *Absolutepath = TokenToString(Token);
+            char *Filename = PluginFilenameFromAbsolutepath(Absolutepath);
+            if(Filename)
+            {
                 struct stat Buffer;
                 if(stat(Absolutepath, &Buffer) == 0)
                 {
@@ -51,33 +109,6 @@ DAEMON_CALLBACK(DaemonCallback)
                     fprintf(stderr, "chunkwm: plugin '%s' not found..\n", Absolutepath);
                 }
             }
-
-            free(Filename);
-        }
-        else
-        {
-            char *Absolutepath = TokenToString(Token);
-            char *LastSlash = strrchr(Absolutepath, '/');
-
-            if(LastSlash)
-            {
-                char *Extension = strrchr(Absolutepath, '.');
-                if((Extension) && (strcmp(Extension, ".so") == 0))
-                {
-                    char *Filename = LastSlash + 1;
-
-                    struct stat Buffer;
-                    if(stat(Absolutepath, &Buffer) == 0)
-                    {
-                        LoadPlugin(Absolutepath, Filename);
-                    }
-                    else
-                    {
-                        fprintf(stderr, "chunkwm: plugin '%s' not found..\n", Absolutepath);
-                    }
-                }
-            }
-
             free(Absolutepath);
         }
     }
@@ -88,42 +119,23 @@ DAEMON_CALLBACK(DaemonCallback)
         if(Directory)
         {
             char *Filename = TokenToString(Token);
-            char *Extension = strrchr(Filename, '.');
-
-            if((Extension) && (strcmp(Extension, ".so") == 0))
+            char *Absolutepath = PluginAbsolutepathFromDirectory(Filename, Directory);
+            if(Absolutepath)
             {
-                size_t DirectoryLength = strlen(Directory);
-                size_t FilenameLength = strlen(Filename);
-                size_t TotalLength = DirectoryLength + 1 + FilenameLength;
-
-                char Absolutepath[TotalLength + 1];
-                memcpy(Absolutepath, Directory, DirectoryLength);
-                memset(Absolutepath + DirectoryLength, '/', 1);
-                memcpy(Absolutepath + DirectoryLength + 1, Filename, FilenameLength);
-
                 UnloadPlugin(Absolutepath, Filename);
+                free(Absolutepath);
             }
+            free(Filename);
         }
         else
         {
             char *Absolutepath = TokenToString(Token);
-            char *LastSlash = strrchr(Absolutepath, '/');
-
-            if(LastSlash)
+            char *Filename = PluginFilenameFromAbsolutepath(Absolutepath);
+            if(Filename)
             {
-                char *Extension = strrchr(Absolutepath, '.');
-                if((Extension) && (strcmp(Extension, ".so") == 0))
-                {
-                    char *Filename = LastSlash + 1;
-                    UnloadPlugin(Absolutepath, Filename);
-                }
+                UnloadPlugin(Absolutepath, Filename);
             }
+            free(Absolutepath);
         }
-    }
-    else if(TokenEquals(Type, CVAR_PLUGIN_DIR))
-    {
-        token Token = GetToken(&Message);
-        char *Directory = TokenToString(Token);
-        UpdateCVar(CVAR_PLUGIN_DIR, Directory);
     }
 }
