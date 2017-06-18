4c76481685f4f17aba9703970445cb5f3c2f8b7b
koekeishiya <aasvi93@hotmail.com>
2017-06-18 22:04:13 +0200

    move osx_event_mask to common

--- src/plugins/ffm/plugin.cpp
+++ src/plugins/ffm/plugin.cpp
@@ -176,25 +176,6 @@ void FocusWindowBelowCursor(CGPoint Cursor)
     CFRelease(ApplicationRef);
 }
 
-enum osx_event_mask
-{
-    Event_Mask_Alt = 0x00080000,
-    Event_Mask_LAlt = 0x00000020,
-    Event_Mask_RAlt = 0x00000040,
-
-    Event_Mask_Shift = 0x00020000,
-    Event_Mask_LShift = 0x00000002,
-    Event_Mask_RShift = 0x00000004,
-
-    Event_Mask_Cmd = 0x00100000,
-    Event_Mask_LCmd = 0x00000008,
-    Event_Mask_RCmd = 0x00000010,
-
-    Event_Mask_Control = 0x00040000,
-    Event_Mask_LControl = 0x00000001,
-    Event_Mask_RControl = 0x00002000,
-};
-
 EVENTTAP_CALLBACK(EventTapCallback)
 {
     event_tap *EventTap = (event_tap *) Reference;
