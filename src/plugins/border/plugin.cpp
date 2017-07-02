062f23757bbada73c6fa787ef1f00a66fea87c26
koekeishiya <aasvi93@hotmail.com>
2017-07-02 19:47:32 +0200

    border 0.2.0

--- src/plugins/border/plugin.cpp
+++ src/plugins/border/plugin.cpp
@@ -306,4 +306,4 @@ chunkwm_plugin_export Subscriptions[] =
 CHUNKWM_PLUGIN_SUBSCRIBE(Subscriptions)
 
 // NOTE(koekeishiya): Generate plugin
-CHUNKWM_PLUGIN("Border", "0.1.0")
+CHUNKWM_PLUGIN("Border", "0.2.0")
