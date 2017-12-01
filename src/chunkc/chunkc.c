c1b2ac71ececf965954aa07439d113b74783c320
koekeishiya <aasvi93@hotmail.com>
2017-12-01 14:06:22 +0100

    change codestyle of chunkc

--- src/chunkc/chunkc.c
+++ src/chunkc/chunkc.c
@@ -15,8 +15,7 @@
 
 int main(int Argc, char **Argv)
 {
-    if(Argc < 2)
-    {
+    if (Argc < 2) {
         fprintf(stderr, "chunkc: no arguments found!\n");
         exit(1);
     }
@@ -24,12 +23,9 @@ int main(int Argc, char **Argv)
     int Port;
 
     char *PortEnv = getenv("CHUNKC_SOCKET");
-    if(PortEnv)
-    {
+    if (PortEnv) {
         sscanf(PortEnv, "%d", &Port);
-    }
-    else
-    {
+    } else {
         Port = FALLBACK_PORT;
     }
 
@@ -37,8 +33,7 @@ int main(int Argc, char **Argv)
     struct sockaddr_in SrvAddr;
     struct hostent *Server;
 
-    if((SockFD = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
+    if ((SockFD = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         fprintf(stderr, "chunkc: could not create socket!\n");
         exit(1);
     }
@@ -49,8 +44,7 @@ int main(int Argc, char **Argv)
     memcpy(&SrvAddr.sin_addr.s_addr, Server->h_addr, Server->h_length);
     memset(&SrvAddr.sin_zero, '\0', 8);
 
-    if(connect(SockFD, (struct sockaddr*) &SrvAddr, sizeof(struct sockaddr)) == -1)
-    {
+    if (connect(SockFD, (struct sockaddr*) &SrvAddr, sizeof(struct sockaddr)) == -1) {
         fprintf(stderr, "chunkc: connection failed!\n");
         exit(1);
     }
@@ -58,8 +52,7 @@ int main(int Argc, char **Argv)
     size_t MessageLength = Argc - 1;
     size_t Argl[Argc];
 
-    for(size_t Index = 1; Index < Argc; ++Index)
-    {
+    for (size_t Index = 1; Index < Argc; ++Index) {
         Argl[Index] = strlen(Argv[Index]);
         MessageLength += Argl[Index];
     }
@@ -67,20 +60,16 @@ int main(int Argc, char **Argv)
     char Message[MessageLength];
     char *Temp = Message;
 
-    for(size_t Index = 1; Index < Argc; ++Index)
-    {
+    for (size_t Index = 1; Index < Argc; ++Index) {
         memcpy(Temp, Argv[Index], Argl[Index]);
         Temp += Argl[Index];
         *Temp++ = ' ';
     }
     *(Temp - 1) = '\0';
 
-    if(send(SockFD, Message, MessageLength, 0) == -1)
-    {
+    if (send(SockFD, Message, MessageLength, 0) == -1) {
         fprintf(stderr, "chunkc: failed to send data!\n");
-    }
-    else
-    {
+    } else {
         // NOTE(koekeishiya): Should we read and print a response ??
         struct timeval TimeVal;
         fd_set ReadFDs;
@@ -92,13 +81,11 @@ int main(int Argc, char **Argv)
         FD_SET(SockFD, &ReadFDs);
 
         select(SockFD + 1, &ReadFDs, NULL, NULL, &TimeVal);
-        if(FD_ISSET(SockFD, &ReadFDs))
-        {
+        if (FD_ISSET(SockFD, &ReadFDs)) {
             char Response[BUFSIZ];
             int BytesRead;
 
-            while((BytesRead = recv(SockFD, Response, sizeof(Response) - 1, 0)) > 0)
-            {
+            while ((BytesRead = recv(SockFD, Response, sizeof(Response) - 1, 0)) > 0) {
                 Response[BytesRead] = '\0';
                 printf("%s", Response);
                 fflush(stdout);
