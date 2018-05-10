2db5cb70083c54d21c1a96b78e4cba08dac4381a
koekeishiya <aasvi93@hotmail.com>
2018-05-10 23:31:10 +0200

    bar: more horrible test-code

--- src/plugins/bar/plugin.cpp
+++ src/plugins/bar/plugin.cpp
@@ -31,7 +31,9 @@ internal const char *plugin_name = "bar";
 internal const char *plugin_version = "0.0.1";
 internal chunkwm_api api;
 
-internal struct cgl_window window;
+internal struct cgl_window mid_window;
+internal struct cgl_window left_window;
+internal struct cgl_window right_window;
 internal pthread_t bar_thread;
 internal bool quit;
 
@@ -130,39 +132,23 @@ float hexf(uint32_t hex)
     return hex / 255.0f;
 }
 
-internal char *focused_application;
-void *BarMainThreadProcedure(void*)
+struct window_render_state
 {
-    CGLError cgl_err;
-    GLenum gl_err;
-
-    GLint cgl_major, cgl_minor;
+    struct  cgl_window *window;
     GLuint vao, vbo, tex;
+};
 
-    struct shader text_shader;
-    GLfloat color1[4] = {hexf(0xcd), hexf(0x95), hexf(0x0d), 1};
-    GLfloat color2[4] = {hexf(0xd7), hexf(0x5f), hexf(0x5f), 1};
-    GLfloat color3[4] = {hexf(0x22), hexf(0xc3), hexf(0xa1), 1};
-
-    float sx = 2.0 / window.width;
-    float sy = 2.0 / window.height;
-
-    cgl_window_make_current(&window);
-    CGLGetVersion(&cgl_major, &cgl_minor);
-    printf("CGL Version: %d.%d\nOpenGL Version: %s\n",
-           cgl_major, cgl_minor, glGetString(GL_VERSION));
-
-    // NOTE(koekeishiya): wireframe mode
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    shader_init_buffer(&text_shader, vertex_shader_code, fragment_shader_code);
+internal void
+init_gl_for_window(struct window_render_state *render_state)
+{
+    cgl_window_make_current(render_state->window);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     glActiveTexture(GL_TEXTURE0);
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
+    glGenTextures(1, &render_state->tex);
+    glBindTexture(GL_TEXTURE_2D, render_state->tex);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -170,14 +156,47 @@ void *BarMainThreadProcedure(void*)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1, &render_state->vao);
+    glGenBuffers(1, &render_state->vbo);
 
-    glBindVertexArray(vao);
+    glBindVertexArray(render_state->vao);
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, render_state->vbo);
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
     glBindVertexArray(0);
+}
+
+internal char *focused_application;
+void *BarMainThreadProcedure(void*)
+{
+    GLint cgl_major, cgl_minor;
+    CGLError cgl_err;
+    GLenum gl_err;
+
+    struct shader text_shader;
+    GLfloat color1[4] = {hexf(0xcd), hexf(0x95), hexf(0x0d), 1};
+    GLfloat color2[4] = {hexf(0xd7), hexf(0x5f), hexf(0x5f), 1};
+    GLfloat color3[4] = {hexf(0x22), hexf(0xc3), hexf(0xa1), 1};
+
+    struct window_render_state left_state = { &left_window };
+    init_gl_for_window(&left_state);
+    shader_init_buffer(&text_shader, vertex_shader_code, fragment_shader_code);
+
+    struct window_render_state mid_state = { &mid_window };
+    init_gl_for_window(&mid_state);
+    shader_init_buffer(&text_shader, vertex_shader_code, fragment_shader_code);
+
+    struct window_render_state right_state = { &right_window };
+    init_gl_for_window(&right_state);
+    shader_init_buffer(&text_shader, vertex_shader_code, fragment_shader_code);
+
+    CGLGetVersion(&cgl_major, &cgl_minor);
+    printf("CGL Version: %d.%d\nOpenGL Version: %s\n",
+           cgl_major, cgl_minor, glGetString(GL_VERSION));
+
+    // NOTE(koekeishiya): wireframe mode
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 
     AXUIElementRef application = AXLibGetFocusedApplication();
     if (application) {
@@ -191,31 +210,75 @@ void *BarMainThreadProcedure(void*)
 
     char buffer[128];
     float width_of_buffer;
+    float sx;
+    float sy;
 
     while (!quit) {
+        // left window
+        sx = 2.0f / left_window.width;
+        sy = 2.0f / left_window.height;
+        cgl_window_make_current(&left_window);
+
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader_enable(&text_shader);
-        glBindVertexArray(vao);
-
-        if (focused_application) {
-            width_of_buffer = width_of_rendered_text(focused_application, sx);
-            render_text(focused_application, -0.95, -0.3f, sx, sy, color1);
-        }
+        glBindVertexArray(left_state.vao);
 
         get_current_date(buffer, sizeof(buffer));
         width_of_buffer = width_of_rendered_text(buffer, sx);
         render_text(buffer, 0.0f - width_of_buffer / 2.0f, -0.3f, sx, sy, color2);
 
-        get_current_time(buffer, sizeof(buffer));
-        width_of_buffer = width_of_rendered_text(buffer, sx);
-        render_text(buffer, 0.95f - width_of_buffer, -0.3f, sx, sy, color3);
+        glBindVertexArray(0);
+        shader_disable();
+
+        // mid window
+        sx = 2.0f / mid_window.width;
+        sy = 2.0f / mid_window.height;
+        cgl_window_make_current(&mid_window);
+
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader_enable(&text_shader);
+        glBindVertexArray(mid_state.vao);
+
+        if (focused_application) {
+            width_of_buffer = width_of_rendered_text(focused_application, sx);
+            render_text(focused_application, 0.0f - width_of_buffer / 2.0f, -0.3f, sx, sy, color1);
+        }
 
         glBindVertexArray(0);
         shader_disable();
 
-        if ((cgl_err = cgl_window_flush(&window)) != kCGLNoError) {
+        // right window
+        sx = 2.0f / right_window.width;
+        sy = 2.0f / right_window.height;
+        cgl_window_make_current(&right_window);
+
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader_enable(&text_shader);
+        glBindVertexArray(right_state.vao);
+
+        get_current_time(buffer, sizeof(buffer));
+        width_of_buffer = width_of_rendered_text(buffer, sx);
+        render_text(buffer, 0.0f - width_of_buffer / 2.0f, -0.3f, sx, sy, color3);
+
+        glBindVertexArray(0);
+        shader_disable();
+
+        // flush all windows
+        if ((cgl_err = cgl_window_flush(&left_window)) != kCGLNoError) {
+            printf("CGL Error: %d\n", cgl_err);
+        }
+
+        if ((cgl_err = cgl_window_flush(&mid_window)) != kCGLNoError) {
+            printf("CGL Error: %d\n", cgl_err);
+        }
+
+        if ((cgl_err = cgl_window_flush(&right_window)) != kCGLNoError) {
             printf("CGL Error: %d\n", cgl_err);
         }
 
@@ -226,8 +289,12 @@ void *BarMainThreadProcedure(void*)
         sleep(1);
     }
 
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &left_state.vao);
+    glDeleteBuffers(1, &left_state.vbo);
+    glDeleteVertexArrays(1, &mid_state.vao);
+    glDeleteBuffers(1, &mid_state.vbo);
+    glDeleteVertexArrays(1, &right_state.vao);
+    glDeleteBuffers(1, &right_state.vbo);
 
     return NULL;
 }
@@ -250,8 +317,14 @@ PLUGIN_BOOL_FUNC(PluginInit)
 
     CreateCVar("bar_font", "/Library/Fonts/Georgia.ttf");
 
+    int left_x = 15;
+    int left_width = 100;
+
+    int right_x = 1805;
+    int right_width = 100;
+
     int x = 510;
-    int y = 10;
+    int y = 15;
     int width = 900;
     int height = 40;
     int font_size = 18;
@@ -266,12 +339,22 @@ PLUGIN_BOOL_FUNC(PluginInit)
         return false;
     }
 
-    if (!cgl_window_init(&window, x, y, width, height, kCGMaximumWindowLevelKey, CGL_WINDOW_GL_CORE, 1)) {
+    if (!cgl_window_init(&left_window, left_x, y, left_width, height, kCGMaximumWindowLevelKey, CGL_WINDOW_GL_CORE, 1)) {
+        return false;
+    }
+
+    if (!cgl_window_init(&mid_window, x, y, width, height, kCGMaximumWindowLevelKey, CGL_WINDOW_GL_CORE, 1)) {
+        return false;
+    }
+
+    if (!cgl_window_init(&right_window, right_x, y, right_width, height, kCGMaximumWindowLevelKey, CGL_WINDOW_GL_CORE, 1)) {
         return false;
     }
 
     FT_Set_Pixel_Sizes(face, 0, font_size);
-    cgl_window_set_sticky(&window, 1);
+    cgl_window_set_sticky(&left_window, 1);
+    cgl_window_set_sticky(&mid_window, 1);
+    cgl_window_set_sticky(&right_window, 1);
     pthread_create(&bar_thread, NULL, &BarMainThreadProcedure, NULL);
     return true;
 }
@@ -280,7 +363,9 @@ PLUGIN_VOID_FUNC(PluginDeInit)
 {
     quit = true;
     pthread_join(bar_thread, NULL);
-    cgl_window_destroy(&window);
+    cgl_window_destroy(&left_window);
+    cgl_window_destroy(&mid_window);
+    cgl_window_destroy(&right_window);
 }
 
 CHUNKWM_PLUGIN_VTABLE(PluginInit, PluginDeInit, PluginMain)
